d69b160daa83c79b1dafbf0edbf5ba8517e82dd4
Jorge G <jorgegomezweb@gmail.com>
2022-10-13 17:03:15 +0200

    feat: secure bot in groups

--- functions/bot/on/index.ts
+++ functions/bot/on/index.ts
@@ -5,6 +5,12 @@ import { MountMap } from 'telegraf/typings/telegram-types';
 export const getUserGreeting = async (
   ctx: NarrowedContext<Context, MountMap['new_chat_members']>
 ): Promise<Message.TextMessage> => {
+  // Prevent add the bot to another group
+  if (ctx.chat.id !== -879681710) {
+    await ctx.reply('El bot se salió del grupo');
+    await ctx.telegram.leaveChat(ctx.message.chat.id);
+  }
+
   let greeting = '';
 
   for (const user of ctx.message?.new_chat_members) {
